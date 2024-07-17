cade58ea05325206893fe1c3893d2b8d05fe08ba
agayevilkin <agayevilkin76@gmail.com>
2024-07-17 05:57:18 +0400

    inital commit

---
+++ src/main/java/com/example/xslfo/XslFoApplication.java
@@ -0,0 +1,13 @@
+package com.example.xslfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XslFoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XslFoApplication.class, args);
+    }
+
+}
